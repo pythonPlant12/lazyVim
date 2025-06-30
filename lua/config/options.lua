092981293d5a7ea5297db8d432fe61b90ebb61d5
Nikita <pythonplant12@gmail.com>
2025-06-30 09:49:26 +0200

    Added cursor to be the block in all the modes

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,3 +8,6 @@ vim.cmd([[let &t_Cs = "\e[4:0m"]])
 vim.g.autoformat = false
 vim.g.lazyvim_python_lsp = "pyright"
 -- vim.g.lazyvim_python_ruff = "ruff_lsp"
+
+-- Cursor configuration - always use block cursor in all modes
+vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:block,r-cr-o:block"
