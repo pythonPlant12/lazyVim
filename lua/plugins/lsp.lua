90ba7d4ab854631c243196cbdf70714b4a9c8a79
nikita-n-dream <nikita@n-dream.com>
2026-03-03 16:41:09 +0100

    Keymaps, ui adjustments, lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -13,4 +13,23 @@ return {
       })
     end,
   },
+  {
+    "neovim/nvim-lspconfig",
+    init = function()
+      local max_width = math.floor(vim.o.columns * 0.5)
+      local max_height = math.floor(vim.o.lines * 0.3)
+
+      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+        border = "rounded",
+        max_width = max_width,
+        max_height = max_height,
+      })
+
+      vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+        border = "rounded",
+        max_width = max_width,
+        max_height = max_height,
+      })
+    end,
+  },
 }
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,8 +1,18 @@
 return {
+  {
+    "LazyVim/LazyVim",
+    init = function()
+      vim.o.winborder = "rounded"
+    end,
+  },
   -- neo-tree: always show hidden files
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
+      popup_border_style = "rounded",
+      window = {
+        position = "left",
+      },
       filesystem = {
         filtered_items = {
           visible = true,
@@ -22,8 +32,92 @@ return {
     opts = {
       options = {
         mode = "tabs",
+        separator_style = "slant",
         show_buffer_close_icons = false,
         show_close_icon = false,
+      },
+    },
+  },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      input = {
+        win = {
+          border = "rounded",
+        },
+      },
+      picker = {
+        win = {
+          input = {
+            border = "rounded",
+          },
+          list = {
+            border = "rounded",
+          },
+          preview = {
+            border = "rounded",
+          },
+        },
+      },
+      notifier = {
+        style = "compact",
+      },
+    },
+  },
+  {
+    "folke/noice.nvim",
+    opts = {
+      presets = {
+        lsp_doc_border = true,
+      },
+      views = {
+        cmdline_popup = {
+          border = {
+            style = "rounded",
+          },
+        },
+        popupmenu = {
+          border = {
+            style = "rounded",
+          },
+        },
+        hover = {
+          border = {
+            style = "rounded",
+          },
+        },
+      },
+    },
+  },
+  {
+    "folke/which-key.nvim",
+    opts = {
+      win = {
+        border = "rounded",
+      },
+    },
+  },
+  {
+    "folke/trouble.nvim",
+    opts = {
+      win = {
+        border = "rounded",
+      },
+    },
+  },
+  {
+    "folke/lazy.nvim",
+    opts = {
+      ui = {
+        border = "rounded",
+      },
+    },
+  },
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ui = {
+        border = "rounded",
       },
     },
   },
@@ -34,6 +128,8 @@ return {
     opts = {
       options = {
         theme = "solarized_dark",
+        section_separators = { left = "", right = "" },
+        component_separators = { left = "", right = "" },
       },
     },
   },
