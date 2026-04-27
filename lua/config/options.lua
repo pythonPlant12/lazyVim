3237691d2fb804ef2a2f499e3b0eae8cf195ab30
nikita-n-dream <nikita@n-dream.com>
2026-04-27 14:32:31 +0200

    fix: disable cursor blink via guicursor a:blinkwait0-blinkon0-blinkoff0

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,6 +4,7 @@
 
 vim.o.smoothscroll = false
 vim.o.cursorline = true
+vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20,a:blinkwait0-blinkon0-blinkoff0"
 
 vim.o.winborder = "rounded"
 vim.o.switchbuf = "useopen"
