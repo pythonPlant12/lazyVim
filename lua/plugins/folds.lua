e9727e50a994c82dfddd7a7c7fab9b63a11e3a99
nikita-n-dream <nikita@n-dream.com>
2026-04-28 11:37:41 +0200

    fix: use nvim-ufo for reliable folding across all file types

---
+++ lua/plugins/folds.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = "kevinhwang91/promise-async",
+    event = "BufReadPost",
+    opts = {
+      provider_selector = function()
+        return { "treesitter", "indent" }
+      end,
+    },
+    init = function()
+      vim.opt.foldlevel = 99
+      vim.opt.foldlevelstart = 99
+    end,
+  },
+}
