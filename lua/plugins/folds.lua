1336d30fcd7c6732289789e545b5a0f92029cd0e
nikita-n-dream <nikita@n-dream.com>
2026-04-29 10:56:27 +0200

    fix: exclude special filetypes from nvim-ufo folding
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- lua/plugins/folds.lua
+++ lua/plugins/folds.lua
@@ -1,10 +1,25 @@
+local excluded_ft = {
+  ["grug-far"]        = true,
+  ["grug-far-history"] = true,
+  ["grug-far-help"]   = true,
+  ["lazy"]            = true,
+  ["mason"]           = true,
+  ["help"]            = true,
+  ["noice"]           = true,
+  ["Trouble"]         = true,
+  ["dashboard"]       = true,
+  ["neo-tree"]        = true,
+}
+
 return {
   {
     "kevinhwang91/nvim-ufo",
     dependencies = "kevinhwang91/promise-async",
     event = "BufReadPost",
     opts = {
-      provider_selector = function()
+      open_fold_hl_timeout = 0,
+      provider_selector = function(_, filetype)
+        if excluded_ft[filetype] then return "" end
         return { "treesitter", "indent" }
       end,
     },
