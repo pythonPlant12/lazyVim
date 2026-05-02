fa24619998f0735ee567d6e64df58fbeba076d6f
nikita-n-dream <nikita@n-dream.com>
2026-05-02 11:15:19 +0200

    Use gray background for illuminate word highlights instead of underline

--- lua/plugins/illuminate.lua
+++ lua/plugins/illuminate.lua
@@ -12,6 +12,16 @@ return {
     },
     config = function(_, opts)
       require("illuminate").configure(opts)
+
+      local function set_illuminate_hl()
+        local bg = vim.o.background == "light" and "#E2DFE8" or "#3A3A4A"
+        vim.api.nvim_set_hl(0, "IlluminatedWordText",  { bg = bg, underline = false })
+        vim.api.nvim_set_hl(0, "IlluminatedWordRead",  { bg = bg, underline = false })
+        vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = bg, underline = false })
+      end
+
+      set_illuminate_hl()
+      vim.api.nvim_create_autocmd("ColorScheme", { callback = set_illuminate_hl })
     end,
   },
 }
